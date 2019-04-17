eef5cb98d07b0f6960a9f1e3e31fecd356d6ee6f
praneethkumar900 <praneethkumar900@gmail.com>
2019-04-17 17:35:13 -0500

    Base Commit
    Level 1 and player movements done.

---
+++ Aragon Game/Assets/PlayerMovement.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public CharacterController2D controller;
+    public Animator animator;
+    public float runSpeed = 40f;
+    float horizontalMove = 0f;
+    bool jump = false;
+    bool crouch = false;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        horizontalMove = Input.GetAxisRaw("Horizontal")*runSpeed;
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            animator.SetBool("isJumping", true);
+            jump = true;
+            
+        }
+
+        if(Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        }
+
+        else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+		
+	}
+
+    void FixedUpdate()
+    {
+        //Move our character
+        controller.Move(horizontalMove*Time.deltaTime, crouch, jump);
+        jump = false;
+    }
+
+    public void OnLanding()
+    {
+        animator.SetBool("isJumping", false);
+    }
+
+    public void OnCrouching(bool crouch)
+    {
+        animator.SetBool("isCrouching", crouch);
+    }
+
+}
